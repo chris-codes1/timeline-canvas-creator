1687b28798e5bcaa4379a6727de7e38700a49dd0
Chris <>
2025-06-06 15:47:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Timeline Canvas Creator
+
+Quickly create timeline structured canvases in Obsidian, without having to individually place cards yourself.
+
+# How to use this plugin:
+
+1. Press Ctrl+P (or Cmd+P on macOS) to open the Command palette. You can also open it from the Ribbon with Command palette icon
+2. Run the 'Create timeline canvas' command
+3. Enter the desired start date, end date, and interval of the timeline
+4. Click submit
---
+++ main.ts
@@ -0,0 +1,245 @@
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+
+// Remember to rename these classes and interfaces!
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+interface CanvasNode {
+	id: string;
+	type: string;
+	text: string;
+	x: number;
+	y: number;
+	width: number;
+	height: number;
+}
+
+const canvasData: {
+	nodes: CanvasNode[];
+	edges: any[]; // or define a proper type for edges if needed
+} = {
+	nodes: [],
+	edges: [],
+};
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class TimelineCanvasPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+
+		// This creates an icon in the left ribbon.
+		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		// 	// Called when the user clicks the icon.
+		// 	new Notice('This is a notice!');
+		// });
+
+		
+		// // Perform additional things with the ribbon
+		// ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
+
+		// This adds a simple command that can be triggered anywhere
+		this.addCommand({
+			id: 'create-timeline-canvas',
+			name: 'Create timeline canvas',
+			callback: () => {
+				new CreateTimelineModal(this.app, (startYear: number, endYear: number, interval: number) => {
+					//console.log ("Start Year:", startYear, "End Year:", endYear, "Interval:", interval);
+					
+					for (let year = startYear; year <= endYear; year += interval) {
+						//console.log(year)
+					}
+
+					let x = 0;
+					const y = 0;
+					const spacing = 100; // Distance between nodes horizontally
+					
+					for (let year = startYear; year <= endYear; year += interval) {
+						const node: CanvasNode = {
+							id: crypto.randomUUID(), // Generates a unique ID for each node
+							type: "text",
+							text: year.toString(),
+							x: x,
+							y: y,
+							width: 100,
+							height: 50,
+						};
+						canvasData.nodes.push(node);
+						x += spacing; // Move next node to the right
+					};
+
+					const fileName = `New Canvas ${Date.now()}.canvas`;
+					const filePath = fileName; // You can change the folder name
+
+					// Create the canvas file
+					this.app.vault.create(filePath, JSON.stringify(canvasData, null, 2));
+
+					// Open the new canvas in a new tab
+					// const file = this.app.vault.getAbstractFileByPath(filePath);
+					// if (file && file instanceof TFile) {
+					// 	this.app.workspace.getLeaf(true).openFile(file);
+					// }
+
+					const fileContent = JSON.stringify(canvasData, null, 2);
+
+					this.app.vault.create(filePath, fileContent).then((file) => {
+						this.app.workspace.getLeaf(true).openFile(file);
+					}).catch((error) => {
+						console.error("Failed to create canvas file:", error);
+					});
+				}).open();
+			}
+		});
+		// This adds an editor command that can perform some operation on the current editor instance
+		// this.addCommand({
+		// 	id: 'sample-editor-command',
+		// 	name: 'Sample editor command',
+		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
+		// 		console.log(editor.getSelection());
+		// 		editor.replaceSelection('Sample Editor Command');
+		// 	}
+		// });
+		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		// this.addCommand({
+		// 	id: 'open-sample-modal-complex',
+		// 	name: 'Open sample modal (complex)',
+		// 	checkCallback: (checking: boolean) => {
+		// 		// Conditions to check
+		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 		if (markdownView) {
+		// 			// If checking is true, we're simply "checking" if the command can be run.
+		// 			// If checking is false, then we want to actually perform the operation.
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
+
+		// 			// This command will only show up in Command Palette when the check function returns true
+		// 			return true;
+		// 		}
+		// 	}
+		// });
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class CreateTimelineModal extends Modal {
+	constructor(app: App, onSubmit: (startYear: number, endYear: number, interval: number) => void) {
+		super(app);
+			this.setTitle('Create a timeline canvas');
+
+			let startYear = '';
+			let endYear = '';
+			let interval = '';
+		new Setting(this.contentEl)
+			.setName('Start year')
+			.addText((text) =>
+				text.onChange((value) => {
+					startYear = value;
+				}));
+		new Setting(this.contentEl)
+			.setName('End year')
+			.addText((text) =>
+				text.onChange((value) => {
+					endYear = value;
+				}));
+		new Setting(this.contentEl)
+			.setName('Interval')
+			.addText((text) =>
+				text.onChange((value) => {
+					interval = value;
+				}));
+		new Setting(this.contentEl)
+			.addButton((btn) =>
+        btn
+			.setButtonText('Submit')
+			.setCta()
+			.onClick(() => {
+				const numberStartYear = Number(startYear.trim())
+				const numberEndYear = Number(endYear.trim())
+				const numberInterval = Number(interval.trim())
+
+				if (isNaN(numberStartYear) || isNaN(numberEndYear) || isNaN(numberInterval)) {
+					new Notice('One or more of the fields are not valid numbers');
+				} else if (numberEndYear <= numberStartYear) {
+					new Notice('End year is less than or the same as start year');
+				} else if (numberInterval <= 0) {
+					new Notice('Interval needs to be greater than 0');
+				} else if ((numberEndYear - numberStartYear) / numberInterval > 1000) {
+					new Notice('Length of timeline cannot be more than 1000 cards long');
+				} else {
+					this.close();
+					onSubmit(numberStartYear, numberEndYear, numberInterval);
+				}
+			}));
+	}
+
+	onOpen() {
+		// const {contentEl} = this;
+		// contentEl.setText('Woah!');
+	}
+
+	onClose() {
+		// const {contentEl} = this;
+		// contentEl.empty();
+	}
+}
+
+// class SampleSettingTab extends PluginSettingTab {
+// 	plugin: TimelineCanvasPlugin;
+
+// 	constructor(app: App, plugin: TimelineCanvasPlugin) {
+// 		super(app, plugin);
+// 		this.plugin = plugin;
+// 	}
+
+// 	display(): void {
+// 		const {containerEl} = this;
+
+// 		containerEl.empty();
+
+// 		new Setting(containerEl)
+// 			.setName('Setting #1')
+// 			.setDesc('It\'s a secret')
+// 			.addText(text => text
+// 				.setPlaceholder('Enter your secret')
+// 				.setValue(this.plugin.settings.mySetting)
+// 				.onChange(async (value) => {
+// 					this.plugin.settings.mySetting = value;
+// 					await this.plugin.saveSettings();
+// 				}));
+// 	}
+// }
